5993c16147b62e06f9beb8cf0d8ced316592310b
Filipe <falcoefilipe@hotmail.com>
2021-06-08 08:54:21 -0400

    [NEW] Adjustment of design

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,48 +1,24 @@
 <template>
-  <div>
+  <div class="toolbar">
     <v-app-bar
-      color="deep-purple accent-4"
-      dense
+      color="#111"
       dark
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Page title</v-toolbar-title>
+    >  
+      <v-toolbar-title class="toolbar--title">Movie Wachlist</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
+      <v-btn text>
+        Minha lista
+      </v-btn>
+
+      <v-btn text>
+        Já assistidos
       </v-btn>
 
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
-
-      <v-menu
-        left
-        bottom
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item
-            v-for="n in 5"
-            :key="n"
-            @click="() => {}"
-          >
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
     </v-app-bar>
   </div>
 </template>
@@ -52,3 +28,12 @@
         name: 'Toolbar',
     }
 </script>
+
+<style lang="scss">
+.toolbar {
+  .toolbar--title {
+    font-family: 'Roboto';
+  }
+}
+  
+</style>
