09c22d2fd930e61b885c1a148200d4c9fee3c49e
Filipe <falcoefilipe@hotmail.com>
2021-06-09 10:12:03 -0400

    [NEW] Creation window login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,41 @@
 <template>
-    <div>
-        Olá Mundo!
-    </div>
+    <v-container class="login--container">
+        <v-row
+        justify="center"
+        no-gutters
+        fill-height
+        >
+            <v-col md="5">
+                <v-card
+                elevation="0"
+                class="pa-4"
+                >
+                    <v-card-title class="pa-0 mb-6 text-center">Entrar</v-card-title>
+                    <form-login 
+                        @login="loginWithFirebase"
+                    />
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
-    export default {
-        name: 'Home',
+import FormLogin from '../components/FormLogin.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        FormLogin
+    },
+    methods: {
+        goTo(name) {
+            this.$router.push({ name: name })
+        },
+
+        loginWithFirebase(user) {
+            console.log(user);
+        }
     }
+}
 </script>
